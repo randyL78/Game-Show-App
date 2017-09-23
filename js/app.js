c4fe2c8c055f539f408849cfd9a9f37aa10e87ac
RandyL <30959856+randyL78@users.noreply.github.com>
2017-09-23 01:16:27 -0400

    added basic app functionality

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const qwertyElement = document.querySelector("#qwerty");
 	const phraseElement = document.querySelector("#phrase");
 	const startButton = document.querySelector(".btn__reset");
-	const phrases = [	"let sleeping dogs lie",
+	const allPhrases = ["let sleeping dogs lie",
 					 	"home is where the heart is",
 					 	"the itsy bitsy spider",
 					 	"eat your heart out",
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 	// variables
 	let missed = 0;
+	let inGame = false;
 	
 	
 	
@@ -32,28 +33,95 @@ document.addEventListener('DOMContentLoaded', function () {
 	// functions 
 	////////////////////////////////////////////////////////////////////////////////
 	function getRandomPhraseArray(phrases) {
+		const rand = random(phrases.length);
+		const phrase = phrases[rand];
+		let chars = [];
+		for (let char of phrase) {		
+			chars.push(char);
+		}
 		
+		return chars;
+	}
+	
+	function random(upperRange = 100) {
+		const rand = Math.floor(Math.random() * upperRange);
+//		console.log(rand);
+		return rand;
 	}
 	
 	function addPhraseToDisplay(arr) {
-		
+		for (let letter of arr) {
+			if (letter !== " ") {
+				phraseElement.appendChild(createLI(letter.toUpperCase(), "letter"));			
+			} else {
+				phraseElement.appendChild(createLI(letter.toUpperCase(), ""));			
+			}
+		}
 	}
 	
-	function checkLetter() {
-		
+	function checkLetter(key) {
+		const letters = phraseElement.querySelectorAll('li');
+		let hit = false;
+		for (let letter of letters) {
+			if(letter.textContent === key.toUpperCase()) {
+				letter.className += "show";
+				hit = true;
+			}
+		}
+		if (!hit) {
+			missed++;
+			alert(missed);
+		}
+	}
+	
+	// create any element and add text to it
+	function createElement(type, text, name) {
+		const el = document.createElement(type);
+		if (name !== "") {
+			el.className = name;
+		}
+		el.textContent = text;
+		return el;
+	}
+	
+	// creat an li element and add text to it
+	function createLI (text, name) {
+		return createElement('li', text, name);
+	}
+	
+	// load basic game setup
+	function loadGame() {
+		inGame = true;
+		const phraseArray = getRandomPhraseArray(allPhrases);
+		addPhraseToDisplay(phraseArray);		
 	}
 	
 	////////////////////////////////////////////////////////////////////////////////
 	// event listeners
 	////////////////////////////////////////////////////////////////////////////////
-	// Todo add evenlistener to start button click and hide start overlay
-	startButton.addEventListener('click'  => () {
+	
+	// click start button to hidestart overlay and start the game
+	startButton.addEventListener('click', ()  =>  {
 		const overlay = document.querySelector("#overlay");
 		overlay.style.display = "none";
+		loadGame();
 	});
 	
-	
-	////////////////////////////////////////////////////////////////////////////////
-	// main
-	////////////////////////////////////////////////////////////////////////////////
+	// listen for keyboard press when in game mode
+	window.addEventListener('keypress', (e) => {
+		const key = e.key;
+		if (inGame) {
+			const letters = qwertyElement.querySelectorAll('button');
+			for (let count in letters) {
+				if (count !== undefined) {
+					const letter = letters[count];
+					if (letter.textContent === key && letter.className !== "chosen") {
+						letter.className = "chosen";
+						checkLetter(key);
+					}
+				}
+			}
+			
+		}
+	});
 });
